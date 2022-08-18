c45798a9c58d7af2b9697b646d56f03222324af5
seun2029 <alaoabiodun56@gmail.com>
2022-08-18 12:16:11 +0100

    created files

---
+++ project/project.js
@@ -0,0 +1,15 @@
+let menu = document.getElementById("menu")
+
+menu.style.maxHeight = "0px";
+
+function menu(){
+    if(menu.style.maxHeight == "0px")
+{
+    menu.style.maxHeight = "130px"
+}
+
+else
+{
+    menu.style.maxHeight = "0px"
+}
+}
